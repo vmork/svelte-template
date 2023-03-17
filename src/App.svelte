4e8d59e386f7b88d509fb93c86f2ecce5c1d6da1
vmork <vallemork@gmail.com>
2023-03-17 20:37:39 +0100

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	console.log("App.svelte ran");
+	let x = 123;
+</script>
+
+<main>
+	<h1>Hello! {x}</h1>
+</main>
+
+<style lang="scss">
+	h1 {
+		color: rgb(17, 123, 81);
+	}
+</style>
